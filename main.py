23e0c24718e3d827387359faa41d270644593d42
NoahSchiro <noahschiro@rocketmail.com>
2022-10-12 13:31:51 -0400

    Building the main program

---
+++ main.py
@@ -0,0 +1,69 @@
+import cv2
+from cv2 import VideoCapture
+from cv2 import waitKey
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Prepare data generator for standardizing frames before sending them into the model
+data_generator = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization=True)
+
+# Load in our trained model
+MODEL_NAME = 'models/asl_classifier.h5'
+model = load_model(MODEL_NAME)
+
+# TODO: Do we really need a crop?
+# Setting up the input image size and frame crop size
+IMAGE_SIZE = 200
+CROP_SIZE = 400
+
+# Creating list of available classes stored in classes.txt
+classes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "space", "nothing"]
+
+# Grab webcam feed
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    # Capture current frame
+    ret, frame = cap.read()
+
+    # TODO: May want to remove this
+    # Target area where the hand gestures should be
+    cv2.rectangle(frame, (0, 0), (CROP_SIZE, CROP_SIZE), (0, 255, 0), 3)
+    
+    # Preprocessing the frame before input to the model
+    cropped_image = frame[0:CROP_SIZE, 0:CROP_SIZE]
+    resized_frame = cv2.resize(cropped_image, (IMAGE_SIZE, IMAGE_SIZE))
+    reshaped_frame = (np.array(resized_frame)).reshape((1, IMAGE_SIZE, IMAGE_SIZE, 3))
+    frame_for_model = data_generator.standardize(np.float64(reshaped_frame))
+
+    # Predicting the frame
+    # prediction = np.array(model.predict(frame_for_model))
+    # predicted_class = classes[prediction.argmax()]      # Selecting the max confidence index
+
+    # # Preparing output based on the model's confidence.
+    # prediction_probability = prediction[0, prediction.argmax()]
+    # if prediction_probability > 0.5:
+    #     # High confidence
+    #     cv2.putText(frame, '{} - {:.2f}%'.format(predicted_class, prediction_probability * 100), 
+    #                                 (10, 450), 1, 2, (255, 255, 0), 2, cv2.LINE_AA)
+    # elif prediction_probability > 0.2 and prediction_probability <= 0.5:
+    #     # Low confidence
+    #     cv2.putText(frame, 'Maybe {}... - {:.2f}%'.format(predicted_class, prediction_probability * 100), 
+    #                                 (10, 450), 1, 2, (0, 255, 255), 2, cv2.LINE_AA)
+    # else:
+    #     # No confidence
+    #     cv2.putText(frame, classes[-2], (10, 450), 1, 2, (255, 255, 0), 2, cv2.LINE_AA)
+
+    # # Display the image with prediction
+    # cv2.imshow('frame', frame)
+
+    # Press q to quit
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('q'):
+        break
+
+# When everything done, release the capture.
+cap.release()
+cv2.destroyAllWindows()
